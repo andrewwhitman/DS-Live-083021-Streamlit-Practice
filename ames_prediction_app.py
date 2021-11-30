8cc6eeefc96cf18c7b0a542f9a649d278b8524f2
Andrew Whitman <whitman.j.andrew@gmail.com>
2021-11-30 18:04:21 -0500

    update app text

--- ames_prediction_app.py
+++ ames_prediction_app.py
@@ -2,27 +2,33 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+# Headers
 st.write("# Ames Iowa House Price Prediction")
 st.write("## Please enter your house details:")
 
+# user input
 lotarea_input = st.number_input(
     label='Enter lot area, in sqft',
     min_value=1300,
-    max_value=39000)
+    max_value=39000,
+    step=10)
 
 firstfloor_input = st.number_input(
     label='Enter first floor area, in sqft',
     min_value=350,
-    max_value=2220)
+    max_value=2220,
+    step=10)
 
 grlivarea_input = st.number_input(
-    label='Enter ground living area, in sqft',
+    label='Enter above ground living area, in sqft',
     min_value=440,
-    max_value=2975)
+    max_value=2975,
+    step=10)
 
+# model predictions
 loaded_model = pickle.load(open('models/ames-model.sav', 'rb'))
 
-if st.button('Generate Predictions'):
+if st.button('Generate Prediction'):
     test_data = pd.DataFrame(columns=['LotArea', '1stFlrSF', 'GrLivArea'])
 
     test_data = test_data.append(
@@ -32,4 +38,4 @@ if st.button('Generate Predictions'):
 
     prediction = loaded_model.predict(test_data)[0]
 
-    st.write(f'${prediction:,.2f}')
+    st.write(f'Estimated sale price is ${prediction:,.0f}')
